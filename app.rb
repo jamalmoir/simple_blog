8bde0dac5194fcbae3e4a59ceb8d43486cf76c21
Jamal Moir <jamal.moir@gmail.com>
2015-07-09 22:05:50 +0100

    Add routes

--- app.rb
+++ app.rb
@@ -4,4 +4,31 @@ require 'bundler'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
 class SimpleBlog < Sinatra::Base
+  not_found do
+    halt 404, 'Page not found.'
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  get '/new_post' do
+    erb :new_post
+  end
+
+  get '/:id' do
+    erb :single_post
+  end
+
+  get '/:id/edit' do 
+    erb :edit_post
+  end
+
+  get '/:id/delete' do
+    erb :edit_post
+  end
 end
