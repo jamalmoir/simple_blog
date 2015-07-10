0f9292870bd04d835ace04f189a1178ddd6dea74
Jamal Moir <jamal.moir@gmail.com>
2015-07-10 20:52:38 +0100

    Add setup page test

--- test/app_test.rb
+++ test/app_test.rb
@@ -17,9 +17,19 @@ end
 class LoginTest < Test::Unit::TestCase
   include Rack::Test::Methods
   def app() SimpleBlog end
-  
+
   def test_get_login_page
     get '/login'
     assert last_response.ok?
   end
-end 
+end
+
+class SetupTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+  def app() SimpleBlog end
+
+  def test_get_setup_page
+    get '/setup'
+    assert last_response.ok?
+  end
+end
