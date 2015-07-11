10eb66bf5f79c6e7f7157591a9857fa1dc818849
Jamal Moir <jamal.moir@gmail.com>
2015-07-11 12:02:31 +0100

    Add authentication helper methods

--- helpers.rb
+++ helpers.rb
@@ -4,20 +4,21 @@ module Sinatra
   module SimpleBlog
     module Helpers
 
-    def encrypt_sha2(text)
-      Digest::SHA2.hexdigest text
-    end
+      def encrypt_sha2(text)
+        Digest::SHA2.hexdigest text
+      end
 
-    def login?
-      session[:username].nil? ? false : true
-    end
+      def login?
+        session[:username].nil? ? false : true
+      end
 
-    def require_login
-      redirect'/login' unless login?
-    end
+      def require_login
+        redirect'/login' unless login?
+      end
 
-    def username
-      session[:username]
+      def username
+        session[:username]
+      end
     end
   end
 end
